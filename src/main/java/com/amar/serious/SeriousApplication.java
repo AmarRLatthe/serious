3e5503a89ce5e71d9629862dac51481209f9bf34
Amar Latthe <amarlatthe060@gmail.com>
2020-04-11 20:21:05 +0530

    initial commit

---
+++ src/main/java/com/amar/serious/SeriousApplication.java
@@ -0,0 +1,13 @@
+package com.amar.serious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amar/serious/SeriousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amar.serious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
